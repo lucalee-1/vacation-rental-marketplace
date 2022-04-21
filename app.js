8dfbfc21b2f0f35f1125486e35850dbf6bf4d888
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-04-21 17:49:02 +0900

    First commit, basic Express app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+app.listen(3000, () => {
+    console.log("Connected to port 3000");
+} )
