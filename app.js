701ce6ee891b00b11f1f0d3f914a0e628ed67c04
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-04-23 16:40:13 +0900

    Add create new rental page

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Rental = require("./models/rental");
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}))
+
 mongoose.connect("mongodb://localhost:27017/vacation-rental"),
   {
     useNewUrlParser: true,
@@ -28,6 +30,16 @@ app.get("/", (req, res) => {
 app.get("/rentals", async (req, res) => {
   const rentals = await Rental.find({});
   res.render("rentals/allRentals", { rentals });
+});
+
+app.post("/rentals", async (req, res) => {
+  const rental = new Rental(req.body.rental);
+  await rental.save()
+  res.redirect(`/rentals/${rental._id}`)
+});
+
+app.get("/rentals/new", (req, res) => {
+  res.render("rentals/addNew");
 });
 
 app.get("/rentals/:id", async (req, res) => {
