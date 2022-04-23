059f8e358251e4baf21f6397f6ac47386cfbbc9a
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-04-23 16:12:29 +0900

    Add index and details page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const Rental = require('./models/rental');
+const Rental = require("./models/rental");
 
 const app = express();
 
@@ -22,7 +22,18 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
+});
+
+app.get("/rentals", async (req, res) => {
+  const rentals = await Rental.find({});
+  res.render("rentals/allRentals", { rentals });
+});
+
+app.get("/rentals/:id", async (req, res) => {
+  const { id } = req.params;
+  const rental = await Rental.findById(id);
+  res.render("rentals/details", { rental });
 });
 
 app.listen(3000, () => {
